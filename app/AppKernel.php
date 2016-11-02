0aff2f979df0bea332dd4c98e7ead0a61744baf4
Simon Vieille <simon@deblan.fr>
2016-11-02 21:27:53 +0100

    patch

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Ajax\SatisfactionBundle\AjaxSatisfactionBundle(),
         );
